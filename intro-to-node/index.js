5ab0598e3e45cc33d4f21246f52903f077e9efac
shivamp1998 <shivampandey98@gmail.com>
2021-03-28 19:32:55 +0530

    NodeJs

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var mySuperhero = superheroes.random();
+console.log(mySuperhero);
+
+var supervillains = require("supervillains");
+var mySupervillain = supervillains.random();
+console.log(mySupervillain);
