b37ce7955fb37e8660a49b10d694bbc5de1b1e6d
shivamp1998 <shivampandey98@gmail.com>
2021-04-13 11:06:55 +0530

    finish db

--- Mongodb-fuitsProject/app.js
+++ Mongodb-fuitsProject/app.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fruitsDB",{useNewUrlParser: true,useUnifiedTopology: true});
 /* Fruit Schema */
 const fruitSchema = mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true
+  },
   rating: Number,
   review: String
 });
@@ -12,4 +15,60 @@ const fruit = new Fruit({
   rating: 4,
   review: "Sucks granny balls"
 });
-fruit.save();
+const bananas = new Fruit({
+  name: "Banana",
+  rating: 5,
+  review: "not worth a shit!"
+});
+const pinapple = new Fruit({
+  name: "pineapple",
+  rating: 10,
+  review: "best fruit I ever had!"
+});
+// Fruit.insertMany([bananas,pinapple],function(err){
+//   if(err){
+//     console.log("Error!");
+//   }else console.log("Success!");
+// });
+const peach = new Fruit({
+    name: "Peach",
+    rating: 5,
+    review: "Never Eaten"
+});
+
+const peopleSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitSchema,
+});
+const Person = mongoose.model("Person",peopleSchema);
+
+const person1 = new Person({
+  name: "Shivam",
+  age: 22
+});
+const person2 = new Person({
+  name: "John",
+  age: 25,
+  favouriteFruit: peach,
+});
+
+
+Person.updateOne({name: "Shivam"},{favouriteFruit:pinapple},function(err){});
+
+
+// Fruit.insertMany([peach],function(err){
+//   if(err){
+//     console.log("FuckOff");
+//   }else console.log("Don't Fuck off");
+// });
+//Fruit.deleteMany({name: "Peach"},function(err){});
+Fruit.find(function(err,fruits){
+  if(err)console.log(err);
+  else{
+    mongoose.connection.close();
+    for(var i=0;i<fruits.length;i++){
+      console.log(fruits[i].name);
+    }
+  }
+});
