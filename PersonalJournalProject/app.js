5291e4aaf20b0e34a3de0b435f6641801e76bd16
shivamp1998 <shivampandey98@gmail.com>
2021-04-16 23:21:18 +0530

    add Database

--- PersonalJournalProject/app.js
+++ PersonalJournalProject/app.js
@@ -1,9 +1,20 @@
 //jshint esversion:6
 
+/* Adding Database*/
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/journalDB",{useNewUrlParser: true, useUnifiedTopology: true});
+const journalSchema = new mongoose.Schema({
+  topic: String,
+  content: String,
+});
+const Journal = mongoose.model("Journal",journalSchema);
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+
 const posts = [];
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,8 +28,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home.ejs",{homeContent: homeStartingContent, content: posts  });
-
+  Journal.find({},function(err,posts){
+      res.render("home.ejs",{homeContent: homeStartingContent, content: posts  });
+  });
 });
 app.get("/about",function(req,res){
   res.render("about.ejs",{aboutpage: aboutContent});
@@ -31,25 +43,31 @@ app.get("/compose",(req,res)=>{
 });
 app.use(bodyParser.urlencoded({extended: true}));
 app.post("/compose",(req,res)=>{
-   const post = {
-     title: req.body.titleText,
-     body: req.body.postText
-   };
-   posts.push(post);
+
+   const post = new Journal({
+     topic: req.body.titleText,
+     content:req.body.postText,
+   });
+   Journal.find({topic: req.body.titleText},function(err,results){
+     if(results.length == 0){
+       post.save();
+     }
+   });
    res.redirect("/");
 });
 app.get("/post/:postName",function(req,res){
   var paramTitle = req.params.postName;
   paramTitle = _.lowerCase(paramTitle);
   var postContent;
-  for(var i=0;i<posts.length;i++){
-    var posttitle = posts[i].title;
+  Journal.find({topic: req.params.postName},function(err,posts){
+      console.log(posts);
+    var posttitle = req.params.postName;
     posttitle = _.lowerCase(posttitle);
-    if(paramTitle == posttitle)
-    postContent = posts[i].body;
-  }
-  res.render("post.ejs",{newPostTitle: req.params.postName, postParaGraph: postContent});
-  console.log("Post page");
+    postContent = posts[0].content;
+    console.log(postContent);
+    res.render("post.ejs",{newPostTitle: req.params.postName, postParaGraph: postContent});
+  });
+
 });
 app.listen(3000, function() {
   console.log("Server started on port 3000");
