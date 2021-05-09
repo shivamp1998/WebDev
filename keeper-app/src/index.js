d5bf36527cd563c182ec2e3ceca6515c32358ce7
shivamp1998 <shivampandey98@gmail.com>
2021-05-09 21:55:46 +0530

    keeper - React

--- keeper-app/src/index.js
+++ keeper-app/src/index.js
@@ -1,27 +1,20 @@
-//1. Create a new React app.
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
-
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import notes from "../src/notes";
-function createNote(note){
-    return <Note key={note.key} title={note.title} content={note.content}/>
-}
-ReactDOM.render(<div>
-    <Header/>
-    {notes.map(createNote)}
-    <Footer/>
-</div>, document.getElementById("root"));
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
