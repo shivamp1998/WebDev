f77edfbbc24724a72f48fb43fef99fa636fd0886
shivamp1998 <shivampandey98@gmail.com>
2021-06-17 12:43:49 +0530

    javaScript

--- JavaScript/Fundamentals/index.js
+++ JavaScript/Fundamentals/index.js
@@ -24,26 +24,63 @@
 
 //coding assignment - 2
 
-let dolphins = (96 + 108 + 89)/3;
-let koalas = (88+91+110)/3;
-
-
-if(dolphins > koalas && dolphins >= 100){
-  console.log(`Dolphins win with the avg score of ${dolphins}`);
-}else if(koalas > dolphins && koalas>=100){
-  console.log(`Koalas win with the avg score of ${koalas}`);
-}else{
-  console.log("Match End in Draw since either the score is equal or the avegage score of winning team is less than 100");
-}
+// let dolphins = (96 + 108 + 89)/3;
+// let koalas = (88+91+110)/3;
+//
+//
+// if(dolphins > koalas && dolphins >= 100){
+//   console.log(`Dolphins win with the avg score of ${dolphins}`);
+// }else if(koalas > dolphins && koalas>=100){
+//   console.log(`Koalas win with the avg score of ${koalas}`);
+// }else{
+//   console.log("Match End in Draw since either the score is equal or the avegage score of winning team is less than 100");
+// }
 
 
 //statement in a template literal using ternary operator
 
-let age = 16;
-console.log(`i like to drink ${age >= 18 ? "wine" : "water"}`);
+// let age = 16;
+// console.log(`i like to drink ${age >= 18 ? "wine" : "water"}`);
 
 //coding assignment - 3
 
-let amount = 275;
-let tip = (amount>=50 && amount<=300) ? 0.15 : 0.20;
-console.log(`The Bill is ${amount} and the Tip is ${tip*amount} and the total value to pay for is ${amount+tip*amount}`);
+// let amount = 275;
+// let tip = (amount>=50 && amount<=300) ? 0.15 : 0.20;
+// console.log(`The Bill is ${amount} and the Tip is ${tip*amount} and the total value to pay for is ${amount+tip*amount}`);
+
+ // 'use strict';
+ //  let hasDriversLicense = false;
+ //  const passTest = true;
+ //  if( passTest  === true )
+ //    hasDriversLicense = true;
+ //
+ //  if(hasDriversLIcense) console.log("I Can Drive!");
+ //
+ //
+
+
+ //Years left before Retirement
+
+
+//  const retirement = (currentYear, birthYear,retirementAge) =>  retirementAge-(currentYear-birthYear);
+// console.log(retirement(2021,1998,60));
+
+
+//Assignment - 3
+//
+// const average = (score1,score2,score3) => (score1+score2+score3)/3;
+// const averageForKoalas = average(65,54,49);
+// const averageForDolphis = average(44,23,71);
+//
+//
+// function checkWinner(avgKoalas,avgDolphins){
+//   if(avgKoalas >= 2*avgDolphins){
+//     return ("Koalas Are the Real Winners Today!");
+//   }else if(avgDolphins >= 2*avgKoalas){
+//     return ("Dolphins Are the Real Winners Today!");
+//   }else{
+//     return ("No Real Winners Today!");
+//   }
+// }
+//
+// console.log(checkWinner(averageForKoalas, averageForDolphis));
