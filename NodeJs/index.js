215e1522b6746b149e97b532abe62dc99df0b115
shivamp1998 <shivampandey98@gmail.com>
2021-07-14 14:45:07 +0530

    NodeJS

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,28 +1,14 @@
-var http = require("http");
-http.createServer(function(req,res){
-  res.writeHead(200,{'content-Type' : 'text/html'});
-  res.end("Hello World!");
-}).listen(3000);
+var rect = require("./rectangle")
 
+function solveRect(l,b) {
+  console.log("Solving for Rectangle with l = " + l + " and b  = " + b);
+  rect(l,b,(err,rectangle) => {
+    if(err) {
+      console.log(err.message);
+    }else{
+      console.log(rectangle.area(l,b),rectangle.perimter(l,b));
+    }
+  });
+}
 
-
-/* Taking an Inut */
-
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-readline.question('What is Your name?', name => {
-  console.log(`hi my name is ${name}`);
-  readline.close();
-})
-
-const progressBar = require("progress");
-const bar = new progressBar(':bar',{total : 10});
-const timer = setInterval(()=> {
-  bar.tick();
-  if(bar.complete){
-    clearInterval(timer);
-  }
-},100);
+solveRect(2,4);
