204bd7c6f667feb3700ad0dcf64a872320fb3bfc
shivamp1998 <shivampandey98@gmail.com>
2021-07-16 19:32:25 +0530

    NodeJS

--- NodeJs/node_express/index.js
+++ NodeJs/node_express/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+const bodyParser = require('body-parser');
 
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use((req,res)=>{
   res.statusCode = 200;
@@ -10,7 +12,16 @@ app.use((req,res)=>{
   res.end("<html><body><h1>This is express server </h1></body></html>");
 })
 
+
+app.all('/dishes',(req,res,next)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type','text/plain');
+  next();
+});
+app.get('/dishes',(req,res)=> {
+  res.end(" Will send all the dishes to you?");
+})
 const server = http.createServer(app);
 server.listen(3000,()=>{
-  console.log("Server Started at port 3000");
+  console.log("Server Started at port 3000!");
 })
