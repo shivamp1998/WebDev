02173084dc88f15b5d0f987f32b606b5589b7660
shivamp1998 <shivampandey98@gmail.com>
2021-07-19 11:04:38 +0530

    NodeJS

--- NodeJs/node_express/index.js
+++ NodeJs/node_express/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use((req,res)=>{
@@ -13,14 +14,22 @@ app.use((req,res)=>{
 })
 
 
-app.all('/dishes',(req,res,next)=>{
+app.all('/dishes',(req,res,next)=> {
   res.statusCode = 200;
   res.setHeader('Content-Type','text/plain');
   next();
 });
-app.get('/dishes',(req,res)=> {
-  res.end(" Will send all the dishes to you?");
-})
+app.get('/dishes',(req,res,next) => {
+  res.end("Will send all the dishes to you!");
+});
+app.post('/dishes',(req,res,next) => {
+  res.end("Will update the dish");
+});
+app.put('/dishes',(req,res,next)=> {
+  res.statusCode = 403;
+  res.end("not supported");
+});
+
 const server = http.createServer(app);
 server.listen(3000,()=>{
   console.log("Server Started at port 3000!");
