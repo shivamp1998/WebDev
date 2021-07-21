8eb45adaa08880bf7797f7f40b324cce22826d3f
shivamp1998 <shivampandey98@gmail.com>
2021-07-21 19:40:04 +0530

    NodeJS

--- NodeJs/node_mongo/index.js
+++ NodeJs/node_mongo/index.js
@@ -1,28 +1,36 @@
-const mongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
+MongoClient.connect(url, (err, client) => {
 
+    assert.equal(err,null);
 
- mongoClient.connect(url,(err,client) => {
-          assert.equal(err,null);
-          const db = client.db(dbname);
-          const collection = db.collection('dishes');
-          collection.insertOne({'name' : 'Apple' , 'description' : 'useless monopoly'},(err,result) => {
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname);
+    const collection = db.collection("dishes");
+    collection.insertOne({"name": "Uthappizza", "description": "test"},
+    (err, result) => {
+        assert.equal(err,null);
+
+        console.log("After Insert:\n");
+        console.log(result.ops);
+
+        collection.find({}).toArray((err, docs) => {
             assert.equal(err,null);
-            console.log("After insert \n");
-            console.log(result.ops);
 
-            collection.find({}).toArray((err,docs)=> {
-              assert.equal(err,null);
-              console.log("Found");
-              console.log(docs);
-              db.dropCollection('dishes',(err,result)=> {
+            console.log("Found:\n");
+            console.log(docs);
+
+            db.dropCollection("dishes", (err, result) => {
                 assert.equal(err,null);
-                client.close();
-              });
-            });
-          });
 
- });
+                client.close();
+            });
+        });
+    });
+
+});
