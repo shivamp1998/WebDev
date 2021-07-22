50a7894aa606fdbb97888eec14afc149bbc813ac
shivamp1998 <shivampandey98@gmail.com>
2021-07-22 16:04:37 +0530

    NodeJS

--- NodeJs/node_mongo/index.js
+++ NodeJs/node_mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -12,25 +13,31 @@ MongoClient.connect(url, (err, client) => {
 
     const db = client.db(dbname);
     const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+       "dishes", (result) => {
+           console.log("Insert Document:\n");
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+           dboper.findDocuments(db, "dishes", (docs) => {
+               console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
+               dboper.updateDocument(db, { name: "Vadonut" },
+                   { description: "Updated Test" }, "dishes",
+                   (result) => {
+                       console.log("Updated Document:\n", result.result);
 
-            console.log("Found:\n");
-            console.log(docs);
+                       dboper.findDocuments(db, "dishes", (docs) => {
+                           console.log("Found Updated Documents:\n", docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+                           db.dropCollection("dishes", (result) => {
+                               console.log("Dropped Collection: ", result);
 
-                client.close();
-            });
-        });
-    });
+                               client.close();
+                           });
+                       });
+                   });
+           });
+   });
+
+
 
 });
