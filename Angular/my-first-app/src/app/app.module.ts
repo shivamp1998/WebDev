c655b4abf163d225a6a14edd32537e9354106072
shivamp1998 <shivampandey98@gmail.com>
2021-07-26 12:45:52 +0530

    Angular

---
+++ Angular/my-first-app/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
