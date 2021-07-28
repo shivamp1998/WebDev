9e86c6bf0997d0d3fac572144a8419f6e0c638f5
shivamp1998 <shivampandey98@gmail.com>
2021-07-28 19:46:14 +0530

    Angular

--- Angular/my-first-app/src/app/app.module.ts
+++ Angular/my-first-app/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
   ],
   imports: [
     BrowserModule
--- Angular/my-first-app/src/app/server/server.component.ts
+++ Angular/my-first-app/src/app/server/server.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
-
+  templateUrl: './server.component.html'
 })
-export class ServerComponent {
 
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  serverstatus() {
+    return "offline";
+  }
 }
