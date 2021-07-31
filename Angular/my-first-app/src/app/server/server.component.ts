37d2022c2313c231efb09b88d3b990dde3779b2b
shivamp1998 <shivampandey98@gmail.com>
2021-07-31 22:34:49 +0530

    Angular

--- Angular/my-first-app/src/app/server/server.component.ts
+++ Angular/my-first-app/src/app/server/server.component.ts
@@ -2,12 +2,24 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+    .hidden {
+      display: hidden;
+    }
+    `
+  ]
 })
 
 export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
+  clicked = false;
+  number  = 1;
+  buttonClicks : Number[] = [];
   constructor() {
     this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
   }
@@ -20,5 +32,10 @@ export class ServerComponent {
     }else {
       return "green";
     }
+  }
+  onClick () {
+    this.number++;
+    this.buttonClicks.push(this.number);
+    this.clicked = this.clicked === false ? true : false;
   }
 }
