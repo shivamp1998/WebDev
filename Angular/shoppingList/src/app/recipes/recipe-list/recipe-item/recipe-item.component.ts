d55e3856f137f3b353a48911fd8bfa6b48466daf
shivamp1998 <shivampandey98@gmail.com>
2021-08-12 20:36:16 +0530

    Angular

--- Angular/shoppingList/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Angular/shoppingList/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+
+  @Input() recipeItem: {name: string, desc:string, imgPath: string}
 
   constructor() { }
 
