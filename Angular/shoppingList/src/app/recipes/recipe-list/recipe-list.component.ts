6626e053a41222aff52cf4f16ac3936b81e9d995
shivamp1998 <shivampandey98@gmail.com>
2021-09-08 17:19:27 +0530

    angular

--- Angular/shoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/shoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 import { Recipe } from  '../recipe.model';
+import {RecipeService} from "../recipe.service";
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A test recipe','This is a simple test','https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
-    new Recipe('A new Test recipe', 'this is another test recipe', 'https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80')
-  ];
+  recipes: Recipe[];
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
   onRecipeSelected(recipe){
       this.recipeWasSelected.emit(recipe);
--- Angular/shoppingList/src/app/recipes/recipe.service.ts
+++ Angular/shoppingList/src/app/recipes/recipe.service.ts
@@ -4,7 +4,7 @@ export class RecipeService{
     new Recipe('A test recipe','This is a simple test','https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
     new Recipe('A new Test recipe', 'this is another test recipe', 'https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80')
   ];
-  getRecipe() {
-    this.recipes.slice();
+  getRecipes() {
+    return this.recipes.slice();
   }
 }
