80c174591d7721b63b3b17b1b7491fc9ac31803e
shivamp1998 <shivampandey98@gmail.com>
2021-12-20 17:31:20 +0530

    angular

--- Angular/shoppingList/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Angular/shoppingList/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -25,7 +25,6 @@ export class RecipeDetailComponent implements OnInit {
   }
   onEditRecipe() {
       this.router.navigate(['edit'], {relativeTo: this.route});
-      console.log("Success!");
   }
 
 }
