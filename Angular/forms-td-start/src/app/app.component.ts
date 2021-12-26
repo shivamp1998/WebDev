9f01c7777cb2315bc58860c21d508448e460725b
shivamp1998 <shivampandey98@gmail.com>
2021-12-26 13:57:03 +0530

    angular

--- Angular/forms-td-start/src/app/app.component.ts
+++ Angular/forms-td-start/src/app/app.component.ts
@@ -9,14 +9,31 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   answer = "";
   genders = ['male','female'];
+  name = "";
+  email = "";
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   answerQ: '',
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
   // onSubmit(form: ElementRef) {
   //   console.log(form.value)
   // }
   onSubmit() {
-    console.log(this.signupForm);
+    console.log(this.signupForm.value);
+    this.name = this.signupForm.value.userData.username;
+    this.email = this.signupForm.value.userData.email;
   }
 }
