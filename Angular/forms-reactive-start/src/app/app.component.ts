640e47ac766c1e933d9275d40a67b08cd6a284d2
shivamp1998 <shivampandey98@gmail.com>
2021-12-28 13:49:50 +0530

    angular

--- Angular/forms-reactive-start/src/app/app.component.ts
+++ Angular/forms-reactive-start/src/app/app.component.ts
@@ -8,21 +8,49 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Shivam', 'Sachin', 'Priyam', 'Sourav'];
+
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null,[Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe((value)=> {
+    //   console.log(value);
+    // })
+    this.signupForm.statusChanges.subscribe((status)=> {
+      console.log(status);
+    });
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: 'shivam',
+    //     email: 'shivampandey98@gmail.com'
+    //   },
+    //   gender: 'female',
+    //   hobbies: ['cricket']
+    // })
+    this.signupForm.patchValue({
+      userData: {
+        username: 'shivam',
+        email: 'shivam@98.com'
+      }
+    })
   }
   onAddHobby() {
     (<FormArray>this.signupForm.get('hobbies')).push(new FormControl(null, Validators.required));
   }
   onSubmit() {
     console.log(this.signupForm.value);
+  }
+  forbiddenNames(control): {[s: string]: boolean}{
+    if(this.forbiddenUsernames.indexOf(control.value)!=-1){
+      return {'UsernameForbidden': true};
+    }
+     return null;
   }
 
 }
