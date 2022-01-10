11f13dae059d3a332cec5014369c812ef06d8dd8
shivamp1998 <shivampandey98@gmail.com>
2022-01-10 20:06:46 +0530

    angular

--- Angular/http-01/src/app/app.component.ts
+++ Angular/http-01/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from "rxjs/operators";
 import { PostService } from './posts.service';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,15 +12,20 @@ export class AppComponent implements OnInit {
   loadedPosts = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
   constructor(private http: HttpClient, private postsService: PostService) {}
 
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe(errorMsg => {
+      this.error = errorMsg;
+    })
     this.isFetching = true;
     this.postsService.fetchPosts().subscribe((posts) => {
       this.isFetching = false;
       this.loadedPosts = posts;
     }, error=> {
-    this.error = error.message;
+      this.isFetching = false;
+      this.error = error.message;
     });
   }
 
@@ -43,6 +48,13 @@ export class AppComponent implements OnInit {
     this.postsService.deletePosts().subscribe( ()=> {
       this.loadedPosts = [];
     })
+
+  }
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
+  }
+  onHandleError() {
+    this.error = null;
   }
 
 }
--- Angular/http-01/src/app/posts.service.ts
+++ Angular/http-01/src/app/posts.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError} from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostService {
+  error = new Subject<string>();
   constructor(private http: HttpClient){}
   createAndStorePosts( title: string, content: string) {
     const postData = {title: title, content: content};
     this.http.post('https://custom-octagon-264317-default-rtdb.firebaseio.com/posts.json', postData).subscribe( responseData => {
       console.log(responseData)
-    })
+    },
+    (error) => {
+      this.error.next(error.message);
+    }
+  )
   }
   fetchPosts() {
     return this.http.get('https://custom-octagon-264317-default-rtdb.firebaseio.com/posts.json').pipe(
@@ -22,6 +28,9 @@ export class PostService {
       }
       return postsArray;
     }));
+    catchError( (errorRes) => {
+      return throwError(errorRes);
+    })
   }
   deletePosts() {
     return this.http.delete('https://custom-octagon-264317-default-rtdb.firebaseio.com/posts.json');
