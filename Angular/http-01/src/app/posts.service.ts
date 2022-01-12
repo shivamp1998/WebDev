e4641dbef895df21524651cb430bc465e95b36e0
shivamp1998 <shivampandey98@gmail.com>
2022-01-12 20:32:33 +0530

    angular

--- Angular/http-01/src/app/posts.service.ts
+++ Angular/http-01/src/app/posts.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { map, catchError} from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpParams , HttpEventType} from '@angular/common/http';
+import { map, catchError, tap} from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostService {
   error = new Subject<string>();
   constructor(private http: HttpClient){}
+
+
+
   createAndStorePosts( title: string, content: string) {
     const postData = {title: title, content: content};
-    this.http.post('https://custom-octagon-264317-default-rtdb.firebaseio.com/posts.json', postData).subscribe( responseData => {
+    this.http.post('https://custom-octagon-264317-default-rtdb.firebaseio.com/posts.json', postData, {
+      observe: 'response'
+    }).subscribe( responseData => {
       console.log(responseData)
     },
     (error) => {
@@ -17,6 +22,10 @@ export class PostService {
     }
   )
   }
+
+
+
+
   fetchPosts() {
     let params = new HttpParams();
     params = params.append('custom','text');
@@ -40,7 +49,23 @@ export class PostService {
       return throwError(errorRes);
     })
   }
+
+
+
   deletePosts() {
-    return this.http.delete('https://custom-octagon-264317-default-rtdb.firebaseio.com/posts.json');
+    return this.http.delete('https://custom-octagon-264317-default-rtdb.firebaseio.com/posts.json', {
+      observe: 'events',
+    }).pipe(tap(event => {
+      if ( event.type == HttpEventType.Response) {
+        console.log("type of event is repsonse");
+        console.log(event.body)
+      }
+      else if ( event.type === HttpEventType.Sent ){
+        console.log("Type of response is sent");
+      }
+      else {
+        console.log("Response is null");
+      }
+    }));
   }
 }
