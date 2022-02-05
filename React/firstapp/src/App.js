0267bc603105ffff1eacdf7ba90de9cf6f7ba11c
shivamp1998 <shivampandey98@gmail.com>
2022-02-05 15:19:56 +0530

    react

---
+++ React/firstapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem'
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
