3461e29fc73c035a703d50216b6430a5267d53fd
shivamp1998 <shivampandey98@gmail.com>
2022-03-17 15:18:10 +0530

    angular

---
+++ d2c/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Train from './components/train';
+function App() {
+  return (
+    <Train/>
+  );
+}
+
+export default App;
---
+++ d2c/src/components/train.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './train.css'
+
+function Train() {
+    return (
+        <div className="container">
+            <div className="bus">
+                <div className='seats'>
+                    <div className='row'>
+                        <p>1</p>
+                    </div>
+                    <div className='row1'>
+                        <p>2</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Train;
