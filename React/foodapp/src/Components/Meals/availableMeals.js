e462d22a674d03a7d5f8b80e08e393a8066a2c88
shivamp1998 <shivampandey98@gmail.com>
2022-04-20 19:43:40 +0530

    react

--- React/foodapp/src/Components/Meals/availableMeals.js
+++ React/foodapp/src/Components/Meals/availableMeals.js
@@ -1,40 +1,50 @@
-import classes from './availableMeals.module.css';
-import Card from '../UI/Card';
+import classes from "./availableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map( meal => <li>{meal.name}</li>);
-    return <section className={classes.meals}>
-        <Card>
-        <ul >
-        {mealsList}
-        </ul>
-        </Card>
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <li>
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    </li>
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-}
+  );
+};
 
 export default AvailableMeals;
---
+++ React/foodapp/src/Components/UI/input.js
@@ -0,0 +1,12 @@
+import classes from './input.module.css';
+
+const Input = props => {
+  return (
+      <div className={classes.input}>
+          <label htmlFor={props.input.id}>{props.label}</label>
+          <input {...props.input}/>
+      </div>
+  )
+}
+
+export default Input;
