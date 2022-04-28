754971863294f4de98900a9449735bebcf856f06
shivamp1998 <shivampandey98@gmail.com>
2022-04-28 23:38:00 +0530

    [React]:changes

--- React/foodapp/src/Components/Layout/HeaderCartButton.js
+++ React/foodapp/src/Components/Layout/HeaderCartButton.js
@@ -7,6 +7,7 @@ const HeaderCardButton = (props) => {
     const cartCtx = useContext(CartContext);
     const numberOfCartItems = cartCtx.items?.reduce((curNumber,item) => {
         return curNumber + item.amount;
+        console.log(curNumber)
     },0);
     console.log(cartCtx)
     return( 
