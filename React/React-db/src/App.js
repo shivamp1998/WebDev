261dd45ed6817a04be7e2f114dd4374ac18bdf91
shivamp1998 <shivampandey98@gmail.com>
2022-05-15 19:00:36 +0530

    rect-db

--- React/React-db/src/App.js
+++ React/React-db/src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-
+import React,{useState} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
-
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
-
+  const [movies,setMovies] = useState([]);
+  const fetchMoviesHandler = async() =>  {
+    fetch('https://swapi.dev/api/films').then((response) => {
+      return response.json();
+    }).then((data) => {
+      const transformedMovies = data.results.map((movies) => {
+        return {
+          id: movies.episode_id,
+          title: movies.title,
+          releaseDate: movies.release_date,
+          openingText: movies.opening_crawl
+        }
+      })
+      setMovies(transformedMovies);
+    })
+    // axios.get('https://swapi.dev/api/films').then((response) => {
+    //   console.log('axios Response' , response);
+    // })
+  }
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies}  />
       </section>
     </React.Fragment>
   );
