005b7b8b2a70c66a196c58d1e6d52e3298ebe102
shivamp1998 <shivampandey98@gmail.com>
2022-05-16 12:43:46 +0530

    counter-changes

--- React/CustomHooks/src/components/ForwardCounter.js
+++ React/CustomHooks/src/components/ForwardCounter.js
@@ -1,18 +1,7 @@
-import { useState, useEffect } from 'react';
-
 import Card from './Card';
-
+import useCounter from '../hooks/user-counter';
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
+  const counter =  useCounter();
   return <Card>{counter}</Card>;
 };
 
