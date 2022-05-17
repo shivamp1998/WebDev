77b0a2d3120541753104ab6c57469f7833de5740
shivamp1998 <shivampandey98@gmail.com>
2022-05-17 14:27:17 +0530

    react

--- React/forms/src/components/SimpleInput.js
+++ React/forms/src/components/SimpleInput.js
@@ -1,14 +1,33 @@
+import { useRef, useState } from 'react'; 
+
 const SimpleInput = (props) => {
+  const [enteredName,setEnteredName] = useState('');
+  const userNameInputReference = useRef();
+  const [nameIsValid,setNameIsValid] = useState(true);
+  const onChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  }
+  const onFormSubmitHandler = (e) => {
+    e.preventDefault();
+    
+    if(enteredName === '') {
+      setNameIsValid(false)
+    }
+    console.log(enteredName)
+    setEnteredName('');
+  }
   return (
     <form>
-      <div className='form-control'>
+      <div className={nameIsValid ? 'form-control' : 'form-control invalid'}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange = {onChangeHandler} ref={userNameInputReference} value={enteredName}/>
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit" onClick={onFormSubmitHandler}>Submit</button>
       </div>
+      { !nameIsValid && <p> Entere Name Cannot be empty </p>}
     </form>
+    
   );
 };
 
