09901b003981dbb92b6c4376a7c409a3af6419e5
shivamp1998 <shivampandey98@gmail.com>
2022-05-21 10:49:09 +0530

    react

--- React/forms/src/components/BasicForm.js
+++ React/forms/src/components/BasicForm.js
@@ -8,35 +8,54 @@ const BasicForm = (props) => {
   const [isEmailTouched,setIsEmailTouched] = useState(false);
   const [isLastNameTouched,setIsLastNameTouched] = useState(false);
   const [isEmailValid,setIsEmailValid] = useState(false);
-  useEffect(() => {
-    if(email === '') {
-      setIsEmailValid(true)
-    }
-  })
+  const [isNameValid,setIsNameValid] = useState(false);
+  const [isLastNameValid, setIsLastNameValid] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
   const formHandler = (e) => {
     e.preventDefault();
+    if(isEmailValid && isNameValid && isLastNameValid) {
+      setIsFormValid(true);
+    }
     console.log(firstName,lastName,email);
     
   }
 
   const emailValidator = (e) => {
-    setIsEmailTouched(true)
+   if(isEmailTouched && e.target.value === '') {
+     setIsEmailValid(false)
+   }
     if(e.target.value.includes('@') && e.target.value.includes('.')) {
       setIsEmailValid(true);
     }else{
       setIsEmailValid(false);
     }
   }
+
+  const nameValidator = (e) => {
+    if(e.target.value === '') {
+      setIsNameValid(false);
+    }else{
+      setIsNameValid(true);
+    }
+  }
+
+  const lastNameValidator = (e) => {
+    if(e.target.value === '') {
+      setIsLastNameValid(false);
+    }else{
+      setIsLastNameValid(true);
+    }
+  }
   return (
     <form>
       <div className='control-group'>
-        <div className={`form-control ${isNameTouched && 'invalid'}`}>
+        <div className={`form-control ${isNameTouched && !isNameValid && 'invalid'}`}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' onChange={e => setFirstName(e.target.value)} onBlur={() => setIsNameTouched(true)}/>
+          <input type='text' id='name' onChange={e => {setFirstName(e.target.value); nameValidator(e)}} onBlur={() => setIsNameTouched(true)}/>
         </div>
-        <div className={`form-control ${isLastNameTouched && 'invalid'}`}>
+        <div className={`form-control ${isLastNameTouched && !isLastNameValid && 'invalid'}`}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' onChange={ e => setLastName(e.target.value)} onBlur={() => setIsLastNameTouched(true)}/>
+          <input type='text' id='name' onChange={ e => {setLastName(e.target.value); lastNameValidator(e)}} onBlur={() => setIsLastNameTouched(true)}/>
         </div>
       </div>
       <div className={`form-control ${!isEmailValid && isEmailTouched && 'invalid'}`}>
