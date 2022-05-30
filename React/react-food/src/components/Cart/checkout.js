02d924d36fb17a40550e6d5bcbadbdda5749a145
shivamp1998 <shivampandey98@gmail.com>
2022-05-30 21:54:08 +0530

    react

---
+++ React/react-food/src/components/Cart/checkout.js
@@ -0,0 +1,10 @@
+import classes from './checkout.module.css';
+const Checkout = () => {
+    return (
+        <form>
+            <div className={classes.control}>
+                <ul></ul>
+            </div>
+        </form>
+    )
+}
