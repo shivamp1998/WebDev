1b45aed6ad9736a69ba51644e9c6b9d3c139e47a
shivamp1998 <shivampandey98@gmail.com>
2022-06-03 16:23:00 +0530

    js

--- JavaScript/arrays/script.js
+++ JavaScript/arrays/script.js
@@ -65,41 +65,106 @@ const accounts = [account1, account2, account3, account4];
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a','b','c','d','e'];
-console.log(arr.slice(2));
+// let arr = ['a','b','c','d','e'];
+// console.log(arr.slice(2));
 //splice
-console.log(arr.splice(2,1));
-console.log(arr);
-arr.reverse();
-console.log(arr)
-let arr2 = ['a','b','c'];
-let letters = arr2.concat(arr);
-console.log(letters);
-letters.forEach((data,index) => {
-  console.log(index,data)
-});
+// console.log(arr.splice(2,1));
+// console.log(arr);
+// arr.reverse();
+// console.log(arr)
+// let arr2 = ['a','b','c'];
+// let letters = arr2.concat(arr);
+// console.log(letters);
+// letters.forEach((data,index) => {
+//   console.log(index,data)
+// });
 
-for(const [index,letter] of letters.entries()) {
-  console.log(`${index} ${letter}`)
-}
+// for(const [index,letter] of letters.entries()) {
+//   console.log(`${index} ${letter}`)
+// }
 
 
-const currency = new Map([
-  ['India','Rupee'],   
-  ['Japan','Yen'],
-  ['USA','USD']
-]);
+// const currency = new Map([
+//   ['India','Rupee'],   
+//   ['Japan','Yen'],
+//   ['USA','USD']
+// ]);
 
-currency.forEach((value,key) => {
-  console.log(key,value)
+// currency.forEach((value,key) => {
+//   console.log(key,value)
+// })
+
+let dogs = [
+  {
+    weight: 22,
+    curFood: 250,
+    owners: ['Alice','Bob']
+  },
+  {
+    weight: 8,
+    curFood: 200,
+    owners: ['Matilda']
+  },
+  {
+    weight: 13,
+    curFood: 275,
+    owners: ['Sarah','John']
+  },
+  {
+    weigth: 32,
+    curFood: 340,
+    owners: ['Micheal']
+  }
+]
+
+dogs = dogs.map((value,index) => {
+  return {
+    ...value,
+    recommendedFood: Number((value.weight ** 0.75) * 28)
+  }
 })
+console.log(dogs)
+
+dogs.map((value,index) => {
+    if(value.owners.includes('Sarah')) {
+      if(value.recommendedFood > value.curFood) {
+        console.log('Eating Too little');
+      }else{
+        console.log('Eating too much');
+      }
+    }
+})
+
+let ownersEatTooMuch = [];
+let ownersEatTooLittle = [];
+
+dogs.map((value,index) => {
+  if(value.recommendedFood < value.curFood){
+    ownersEatTooMuch.push(value.owners);
+  }else{
+    ownersEatTooLittle.push(value.owners)
+  }
+})
+
+console.log(ownersEatTooMuch.flat(),'-',ownersEatTooLittle.flat());
+
+console.log(dogs.some((value) => value.curFood > value.recommendedFood * 0.09 && value.curFood < (value.recommendedFood * 1.10)))
+
+
+const okayAmount = dogs.map((value,index) => {
+  if( value.curFood > value.recommendedFood * 0.09 && value.curFood < (value.recommendedFood * 1.10)){
+    return value.weight;
+  }
+})
+
+console.log(okayAmount);
