ec5daa4cdff786f449fc4fb43728dd197d471f08
shivamp1998 <shivampandey98@gmail.com>
2022-06-06 16:02:10 +0530

    javascript

--- JavaScript/Dates/script.js
+++ JavaScript/Dates/script.js
@@ -251,5 +251,24 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-console.log(Number.parseInt('23px'));
+// console.log(Number.parseInt('23px'));
+
+
+
+const now = new Date();
+console.log(now);
+console.log(new Date('Aug 02 2020'));
+
+const future = new Date(2037, 10,19,15,23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getDate())
+console.log(future.getDay());
+console.log(future.getMonth());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.getMilliseconds());
+console.log(future.getTime());
+
 
