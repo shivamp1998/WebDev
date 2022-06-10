8ffa1947d3c98c25f5c8826eb86927c067972e55
shivamp1998 <shivampandey98@gmail.com>
2022-06-10 13:47:06 +0530

    changes

--- JavaScript/bankist-dom/script.js
+++ JavaScript/bankist-dom/script.js
@@ -32,27 +32,67 @@ document.addEventListener('keydown', function (e) {
 });
 
 
-console.log(document.documentElement);
+// console.log(document.documentElement);
 
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-const allButtons = document.getElementsByTagName('button')
-console.log(allButtons);
+// const allButtons = document.getElementsByTagName('button')
+// console.log(allButtons);
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
 // message.textContent="We use cookies for imporved functionality and analytics."
-message.innerHTML=`we use cookies for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>`
+// message.innerHTML=`we use cookies for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>`
 
 
-const header = document.querySelector('.header');
-// header.append(message);
-header.prepend(message);
+// const header = document.querySelector('.header');
+// // header.append(message);
+// header.prepend(message);
 // header.after(message);
 // header.before(message);
 
-const cookieClose = document.querySelector('.btn--close-cookie').addEventListener('click',(e) => {
-  message.remove();
-})
+// const cookieClose = document.querySelector('.btn--close-cookie').addEventListener('click',(e) => {
+//   message.remove();
+// })
 
+// message.style.backgroundColor = `#37383d`;
+// message.style.height = "120%";
+
+// console.log(getComputedStyle(message).color);
+
+// document.documentElement.style.setProperty('--color-primary','orangered');
+
+/*Attributes*/
+// const link = document.querySelector('.nav img')
+// console.log(link.src);
+// console.log(link.alt);
+// console.log(link.className);
+
+
+// console.log(link.getAttribute('designer'));
+// link.setAttribute('designer','sarvesh');
+
+// console.log(link.dataset.roleVersion)
+
+console.log(document.documentElement.clientHeight);
+console.log(document.documentElement.clientWidth);
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+console.log(section1)
+
+btnScrollTo.addEventListener('click', (e) => {
+  console.log(window.pageXOffset,window.pageYOffset);
+
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  // window.scrollTo(s1coords.x,s1coords.y)
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.y + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+  section1.scrollIntoView({behavior: 'smooth'})
+})
