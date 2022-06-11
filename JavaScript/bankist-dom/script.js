8cfaaba15792e201a2a6045b0f460e6c73b76ae3
shivamp1998 <shivampandey98@gmail.com>
2022-06-11 12:15:52 +0530

    changes

--- JavaScript/bankist-dom/script.js
+++ JavaScript/bankist-dom/script.js
@@ -126,4 +126,19 @@ document.querySelector('.nav__links' ).addEventListener('click',e => {
 })
 
 
- 
+const h1 = document.querySelector('h1');
+
+console.log(h1.childNodes);
+console.log(h1.children);
+
+ h1.firstElementChild.style.color = 'black';
+console.log(h1.querySelectorAll('.highlight'));
+
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "black";
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
