d63822548aecccdc5f20e6b0d8713646d158d80d
shivamp1998 <shivampandey98@gmail.com>
2022-06-28 15:49:33 +0530

    changes

--- JavaScript/OOPS/script.js
+++ JavaScript/OOPS/script.js
@@ -1,19 +1,53 @@
 'use strict';
 
-const Person = function(firstName,birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+// const Person = function(firstName,birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
     
+// }
+
+// const shivam = new Person('shivam','1989');
+// const priyam = new Person('priyam',2003);
+// Person.prototype.calAge = function() {
+//     console.log(2037 - this.birthYear);
+// }
+
+// shivam.calAge()
+// priyam.calAge()
+
+
+
+// console.log(Person.prototype)
+// console.log(shivam.__proto__)
+
+
+// console.log(shivam.__proto__.__proto__);
+// console.log(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor)
+
+
+/* Coding Challenge */
+
+function Car(make,speed) {
+    this.make = make;
+    this.speed = speed;
 }
 
-const shivam = new Person('shivam','1989');
-const priyam = new Person('priyam',2003);
-Person.prototype.calAge = function() {
-    console.log(2037 - this.birthYear);
+Car.prototype.accelerate = function(){
+    this.speed += 10;
+    console.log(this.speed)
 }
 
-shivam.calAge()
-priyam.calAge()
+Car.prototype.break = function(){
+    this.speed -= 10;
+    console.log(this.speed)
+}
 
+const bmw = new Car('bmw',120);
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.break()
+const mercedes = new Car('mercedes',95);
 
-console.log(shivam)
+console.log(bmw.speed)
