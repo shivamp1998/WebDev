a214517404c45d1239b012982b21122cdd5a9954
shivamp1998 <shivampandey98@gmail.com>
2022-07-07 10:29:32 +0530

    changes

---
+++ JavaScript/Async/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const getCountry = function (country) {
+    const request = new XMLHttpRequest();
+    request.open('GET',`https://restcountries.com/v2/name/${country}`);
+    request.send();
+
+
+    request.addEventListener('load',function() {
+        const data = JSON.parse(this.responseText)[0];
+        
+        const html = `
+        <article class="country">
+                <img class="country__img" src="${data.flag}" />
+                <div class="country__data">
+                    <h3 class="country__name">${data.name}</h3>
+                    <h4 class="country__region">${data.region}</h4>
+                    <p class="country__row"><span>👫</span>${data.population} people</p>
+                    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+                </div>
+                </article>
+        `
+        countriesContainer.insertAdjacentHTML('beforeend',html);
+        countriesContainer.style.opacity = 1;
+    })
+
+}
+
+getCountry('india');
+getCountry('Britain')
