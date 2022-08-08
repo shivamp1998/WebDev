03288bbce430b767c0f604807b1a6496cb870afc
shivamp1998 <shivampandey98@gmail.com>
2022-08-08 16:10:49 +0530

    Changes

--- NodeJs/node-farm/server.js
+++ NodeJs/node-farm/server.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs')
 
 const server = http.createServer((req,res) => {
-    console.log(req)
-    res.end("Hello From Server");
+    const pathname = req.url;
+    if(pathname === '/home') {
+        res.end('HOME')
+    }else if(pathname === '/overview') {
+        res.end('Overview')
+    }else if(pathname === '/api') {
+        fs.readFile('./dev-data/data.json', (err,data) => {
+            const parsedData = JSON.parse(data);
+            console.log(parsedData);
+            res.writeHead(200, {
+                'Content-Type' : 'application/json'
+            })
+            res.end(data)
+        })
+    }else{
+        res.writeHead(404, {
+            'Content-Type' : 'text/html'
+        });
+        res.end('<h1>Not Found</h1>')
+    }
 });
 
 
