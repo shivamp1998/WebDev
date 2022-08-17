d4cc11bc32e96884238a0989fe89089dc08638d3
shivamp1998 <shivampandey98@gmail.com>
2022-08-17 18:00:02 +0530

    changes

--- NodeJs/natours/app.js
+++ NodeJs/natours/app.js
@@ -9,6 +9,4 @@ app.use(express.static('public'))
 app.use(express.json());
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/tours',tourRoutes)
-
-
 module.exports = app;
--- NodeJs/natours/server.js
+++ NodeJs/natours/server.js
@@ -1,6 +1,16 @@
 const dotenv = require('dotenv');
 const app = require('./app');
+const mongoose = require('mongoose');
 dotenv.config({path: './config.env'})
+
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true,useUnifiedTopology: true})
+.then((con) => {
+    console.log('Connection established! ',process.env.MONGO_URL);
+})
+
+
+
 app.listen(process.env.port, () => {
     console.log('Server Started on port ' + process.env.port);
 });
