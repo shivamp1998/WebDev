ddafa62e26b65df6e4d0fd6c78d217ef23d99243
shivamp1998 <shivampandey98@gmail.com>
2023-03-10 11:38:47 +0530

    factory

--- NodeJs/natours/controllers/handlerFactory.js
+++ NodeJs/natours/controllers/handlerFactory.js
@@ -12,3 +12,39 @@ exports.deleteOne = Model => async (req,res,next) => {
         next(err);
     }
 }
+
+exports.updateOne = Model => async (req,res,next) => {
+    try {
+        const doc = Model.findOneAndUpdate({_id: req.params.id}, {
+            $set: {
+                ...req.body
+            }
+        })
+        if(!doc) {
+            throw new Error('No such document found!');
+        }
+        return res.status(200).send({
+            success: true,
+            doc: doc
+        })
+    }catch(err) {
+        next(err);
+    }
+}
+
+exports.createOne = Model => async (req,res,next) => {
+    try {
+        const doc = Model.create({
+            ...req.body
+        });
+        if(!doc) {
+            throw new Error('Some Error Occurred');
+        }
+        return res.status(200).send({
+            success: true,
+            doc
+        })
+    }catch(err) {
+        next(err);
+    }
+}
