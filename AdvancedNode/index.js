5354800e18668ffc552d69fe6be15e080a29b81c
shivamp1998 <shivampandey98@gmail.com>
2023-03-16 13:10:26 +0530

    [Fixes]

--- AdvancedNode/index.js
+++ AdvancedNode/index.js
@@ -1,29 +1,18 @@
-const cluster = require("cluster");
+const express = require("express");
+const app = express();
+const crypto = require('crypto');
 
-if (cluster.isMaster) {
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-} else {
-  const express = require("express");
-  const app = express();
 
-  function doWork(duration) {
-    let start = Date.now();
-    while(Date.now() - start  < duration) {}
-  }
-
-  app.get("/", (req, res, next) => {
-    doWork(5000)
+app.get("/", (req, res, next) => {
+  crypto.pbkdf2('a','b', 100000, 512, 'sha512', () => {
     res.end("<h1> Hello World </h1>");
-  });
-
-  app.get('/fast', (req,res,next) => {
-    res.send('<h1> This was Fast </h1>')
   })
+});
 
-  app.listen(3000, () => {
-    console.log("connected at port 3000");
-  });
-}
+app.get("/fast", (req, res, next) => {
+  res.send("<h1> This was Fast </h1>");
+});
+
+app.listen(3000, () => {
+  console.log("connected at port 3000");
+});
