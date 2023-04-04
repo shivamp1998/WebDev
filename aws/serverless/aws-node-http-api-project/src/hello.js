75fe4a48c13da7732401b0cb4a7b5a4a2ab372ee
shivamp1998 <shivampandey98@gmail.com>
2023-04-04 12:11:54 +0530

    [Serverless]

---
+++ aws/serverless/aws-node-http-api-project/src/hello.js
@@ -0,0 +1,17 @@
+const hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports = {
+  handler: hello
+}
