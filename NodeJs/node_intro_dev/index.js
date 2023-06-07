1c886d5ea492a5e5613eb559f472c1883a7ad0a7
shivamp1998 <shivampandey98@gmail.com>
2023-06-07 22:21:02 +0530

    fix

--- NodeJs/node_intro_dev/index.js
+++ NodeJs/node_intro_dev/index.js
@@ -1,9 +1,22 @@
 const cron = require('node-cron');
 let secondsTimer = 1;
 let minutesTimer = 1;
+const Queue = require('bull');
 
 
 cron.schedule('*/2 * * * *', () => {
     console.log('runing every 2 seconds', secondsTimer)
     secondsTimer++;
 })
+
+
+const audioQueue = Queue.create('audioQueue');
+
+audioQueue.process((job, done) => {
+    if(job) {
+        console.log(job);
+    }else{
+        console.log(err);
+    }
+    return done()
+}) 
