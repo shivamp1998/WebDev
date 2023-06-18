a2ecdaef05c4d4efcb5a035e350468196655f847
shivamp1998 <shivampandey98@gmail.com>
2023-06-18 23:50:25 +0530

    fix

--- NodeJs/node_intro_dev/index.js
+++ NodeJs/node_intro_dev/index.js
@@ -54,7 +54,7 @@ const arrayLike = {
 ;
 console.log(Array.prototype.at.call(arrayLike, -1)); // "b"
 //array prototype returns undefined
-
-for(const key of arrayLike) {
-    console.log(key);
-}
+console.log(Array.prototype.concat.call({}, 1, 2, 3)); // [{}, 1, 2, 3]
+console.log(Array.prototype.concat.call(1, 2, 3)); // [ [Number: 1], 2, 3 ]
+const arrayLike = { [Symbol.isConcatSpreadable]: true, length: 2, 0: 1, 1: 2 };
+console.log(Array.prototype.concat.call(arrayLike, 3, 4)); // [1, 2, 3, 4]
