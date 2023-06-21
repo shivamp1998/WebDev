afd58c1b510dbd37bdebde0a4ced14860dc84c59
shivamp1998 <shivampandey98@gmail.com>
2023-06-21 23:00:41 +0530

    fix

--- NodeJs/node_intro_dev/index.js
+++ NodeJs/node_intro_dev/index.js
@@ -58,3 +58,15 @@ console.log(Array.prototype.concat.call({}, 1, 2, 3)); // [{}, 1, 2, 3]
 console.log(Array.prototype.concat.call(1, 2, 3)); // [ [Number: 1], 2, 3 ]
 arrayLike = { [Symbol.isConcatSpreadable]: true, length: 2, 0: 1, 1: 2 };
 console.log(Array.prototype.concat.call(arrayLike, 3, 4)); // [1, 2, 3, 4]
+
+
+
+const array1 = ['a', 'b', 'c', 'd', 'e'];
+
+// Copy to index 0 the element at index 3
+console.log(array1.copyWithin(0, 3, 4));
+// Expected output: Array ["d", "b", "c", "d", "e"]
+
+// Copy to index 1 all elements from index 3 to the end
+console.log(array1.copyWithin(1, 3));
+// Expected output: Array ["d", "d", "e", "d", "e"]
